fe68a6a1b788ff2a1cb7436d6c4f8f1a7a65b781
SounSom <thasotheara911@gmail.com>
2026-06-08 14:44:43 +0700

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
+#include "student.h"
+
+// Prototypes (links to the other files)
+void addStudent();
+void searchStudent();
+void updateStudent();
+void deleteStudent();
+void saveToFile();
+void loadFromFile();
 
 int main() {
-    printf("Welcome to our Student Management System!\n");
+    int choice;
+    loadFromFile(); // Load existing data when program starts
+
+    do {
+        printf("\n--- Student Management System ---\n");
+        printf("1. Add\n2. Search\n3. Update\n4. Delete\n5. Exit\n");
+        printf("Enter choice: ");
+        scanf("%d", &choice);
+
+        switch(choice) {
+            case 1: addStudent(); break;
+            case 2: searchStudent(); break;
+            case 3: updateStudent(); break;
+            case 4: deleteStudent(); break;
+            case 5: saveToFile(); break;
+            default: printf("Invalid choice.\n");
+        }
+    } while(choice != 5);
+
     return 0;
 }
